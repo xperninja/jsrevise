9b483447f0708cf797d57a550aec87c73b54d53f
xperninja <66830177+xperninja@users.noreply.github.com>
2020-09-05 10:48:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var name = "Dhruv";
+console.log(name)
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+var num = 5
+name = parseInt("10")
+console.log(name)  
+var fname = "Dhruv"
+var Sname = "Shukla"
+console.log(Sname)
+console.log(fname +Sname)
+
+var a = 1
+var b = 0
+console.log(a/b)
+var bool = true
+function draw(){
+  if(bool){
+ print(bool)
+  } 
+  for (var i=1;i<=40;i++){
+if (i%2===0){
+text(i,350,10*i) 
+}else{
+  text(i,50,10*i) 
+}
+  } 
+}
